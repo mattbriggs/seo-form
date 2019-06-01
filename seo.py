0c3603399f86c5c157930bdc20463da0907e1668
MattBriggs <matt_d_briggs@hotmail.com>
2019-06-01 08:59:06 -0700

    Adding essential working SEO Form

---
+++ seo.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import seoonpage as SEO
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def student():
+   return render_template('onpage.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      seo_dict=SEO.get_top_ten(result['Markdown'])
+      return render_template("result.html",result = seo_dict)
+
+if __name__ == '__main__':
+   app.run(debug = True)
